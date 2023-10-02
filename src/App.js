69ef04a931b1d8e1df3e9fcbf71c9befbfa25d78
Moulyasree1234 <143620769+Moulyasree1234@users.noreply.github.com>
2023-10-01 21:59:13 -0400

    AddedChartjs and d3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import 
+  {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+  } from "react-router-dom";
+
+import Aboutpage from './AboutPage/AboutPage';
+import Loginpage from './LoginPage/LoginPage';
+import Menu from './Menu/Menu';
+import HomePage from './HomePage/HomePage';
+import Footer from './Footer/Footer';
+import Hero from './Hero/Hero';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router className="App">
+      <Menu/>
+      <Hero/>
+      <div className="main Container">
+        
+        <Routes>
+          <Route path="/About" element={<Aboutpage/>}/>
+          <Route path="/Login" element={<Loginpage/>}/>
+          <Route path="/" element={<HomePage/>} />
+        </Routes>
+      </div>
+      <Footer/>
+    </Router>
   );
 }
+
 
 export default App;
